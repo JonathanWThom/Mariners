7aa13702261b286c550793acaec0e4704017877d
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-01-28 08:21:18 -0800

    api working

--- src/app/stats.service.ts
+++ src/app/stats.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@angular/core';
+//not using response yet
+import { Http, Response } from '@angular/http';
+//using observable or not?
+import { Observable } from 'rxjs/Observable';
+// import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+import { marinersKey } from './api-keys';
 
 @Injectable()
 export class StatsService {
+  private statsUrl = 'http://api.sportradar.us/mlb-t5/seasontd/2016/REG/teams/43a39081-52b4-4f93-ad29-da7f329ea960/statistics.json?api_key=' + marinersKey.key;
+  constructor (private http: Http) {}
 
-  constructor() { }
+  getTeamStats() {
+      return this.http.get(this.statsUrl)
+      .map(res => {
+      return <any[]> res.json();
+      //add failure conditions
+    });
+  }
 
 }
--- src/app/stats/stats.component.ts
+++ src/app/stats/stats.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { StatsService } from '../stats.service';
+//use this?
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-stats',
   templateUrl: './stats.component.html',
-  styleUrls: ['./stats.component.css']
+  styleUrls: ['./stats.component.css'],
+  providers: [StatsService]
 })
 export class StatsComponent implements OnInit {
-
-  constructor() { }
+  //give these object types?
+  stats;
+  players: any = [];
+  constructor(private ss: StatsService) { }
 
   ngOnInit() {
+    this.ss.getTeamStats().subscribe(x => {
+      this.stats = x;
+      this.players = this.stats.players;
+    });
   }
 
 }
